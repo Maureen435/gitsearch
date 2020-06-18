f38b8ce9c9a78d98571bc1ee97fcf2405e6f579c
Maureen435 <mchepkemoi435@gmail.com>
2020-06-18 09:43:16 +0300

    initial commit

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+//import { Http, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+//import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private clientid='62882327';
+  private clientsecret='';
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    console.log("service is now ready!");
+    this.username = 'Maureen435';
+
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?clientid=" + this.clientid + "&client_secret=")
+    ;
+  }
 }
